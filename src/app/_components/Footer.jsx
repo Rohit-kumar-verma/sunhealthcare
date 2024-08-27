5010781e587453d5888f8d4e39071be812a3828a
Rohit Kumar <rohitverma7754@gmail.com>
2024-08-27 23:52:55 +0530

    new commit

--- src/app/_components/Footer.jsx
+++ src/app/_components/Footer.jsx
@@ -4,11 +4,11 @@ import { BiLogoTwitter, BiLogoLinkedin, BiLogoInstagram } from "react-icons/bi";
 
 function Footer() {
   return (
-    <footer className='bg-cover mt-20 px-10 py-4' style={{ backgroundImage: `url(${"footer-bg.jpg"})`}}>
-      <div className='grid lg:grid-cols-5 md:grid-cols-2 sm:grid-cols-1 gap-6 p-6'>
-        <div className='grid grid-flow-row grid-rows-3 gap-3'>
+    <footer className='bg-cover mt-20 px-6 sm:px-10 py-8' style={{ backgroundImage: `url(${"footer-bg.jpg"})`}}>
+      <div className='grid lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-6 p-6'>
+        <div className='space-y-4'>
           <a href='#'>
-            <img src='logo.svg' alt='logo' />
+            <img src='output-onlinegiftools.gif' alt='logo' className='w-46 sm:w-42 md:w-50 lg:w-44' />
           </a>
           <div className='text-white'>
             <p>'Sun Health Care Services', a prominent provider of quality health care services, has been offering a wide range of health care services in Lucknow for an extended period of time.</p>
@@ -44,72 +44,66 @@ function Footer() {
             </li>
           </ul>
         </div>
-        <div className='text-white ml-10'>
-          <div>
-            <h2 className='text-3xl font-semibold'>Useful Links</h2>
-            <ul className='list-none flex flex-col'>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80 ease-in'>
-                <a href='#'>Home</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>About</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Services</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Contact</a>
-              </li>
-            </ul>
-          </div>
+        <div className='text-white ml-14'>
+          <h2 className='text-2xl font-semibold'>Useful Links</h2>
+          <ul className='list-none space-y-2 mt-4'>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Home</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>About</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Services</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Contact</a>
+            </li>
+          </ul>
         </div>
         <div className='text-white'>
-          <div>
-            <h2 className='text-3xl font-semibold'>Our Services</h2>
-            <ul className='list-none flex flex-col'>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Attendent</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Nursing Care</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Boby Care</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Elder Care</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Physiotherapy</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>ICU care</a>
-              </li>
-              <li className='mt-4 hover:text-[#00ADB5] hover:translate-x-4 transition hover:delay-80'>
-                <a href='#'>Medical Equipments on rent</a>
-              </li>
-            </ul>
-          </div>
+          <h2 className='text-2xl font-semibold'>Our Services</h2>
+          <ul className='list-none space-y-2 mt-4'>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Attendant</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Nursing Care</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Body Care</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Elder Care</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Physiotherapy</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>ICU Care</a>
+            </li>
+            <li className='hover:text-[#00ADB5] transition-transform transform hover:translate-x-2'>
+              <a href='#'>Medical Equipments on Rent</a>
+            </li>
+          </ul>
         </div>
         <div className='text-white'>
-          <h1 className='text-2xl'>
+          <h1 className='text-2xl font-semibold'>
             Our Location
           </h1>
-          <div className=''>
-          <iframe
+          <div className='mt-4'>
+            <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6188.070207381763!2d80.9097952032933!3d26.899631763129847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399957005a2becbd%3A0x5c5a5d465205ef05!2sSun%20Hom%20Health%20Services!5e0!3m2!1sen!2sin!4v1724725285420!5m2!1sen!2sin"
-              width="500"
+              width="100%"
               height="300"
               style={{ border: 0 }}
               allowFullScreen=""
               loading="lazy"
             />
-            {/* <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6188.070207381763!2d80.9097952032933!3d26.899631763129847!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x399957005a2becbd%3A0x5c5a5d465205ef05!2sSun%20Hom%20Health%20Services!5e0!3m2!1sen!2sin!4v1724725285420!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"/> */}
-
           </div>
         </div>
       </div>
-      <hr className='border-t border-gray-600' />
+      <hr className='border-t border-gray-600 mt-8' />
     </footer>
   )
 }
