10cb629d83ee71e8c34e2541d400b9bd578e5c34
Rohit Kumar <rohitverma7754@gmail.com>
2024-08-27 23:33:34 +0530

    second commit

--- src/app/_components/Header.jsx
+++ src/app/_components/Header.jsx
@@ -27,7 +27,7 @@ export default function Header() {
           </div>
           <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between h-[50%] flex-shrink">
             <div className="flex flex-shrink-0 items-center">
-              <img src='logo_2.gif' alt='Logo' className='w-[44%]' />
+              <img src='logo_2.gif' alt='Logo' className='w-[44%] mr-4 ml-auto lg:ml-0' />
             </div>
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
